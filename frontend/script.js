544a177dd6ca896ba19dea48188993ad7ce6fe48
windiewhale <feketebodza@gmail.com>
2022-02-03 20:42:39 +0100

    script header, style in css

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-function addTwoIntegers(firstInteger, secondInteger) {
+/* function addTwoIntegers(firstInteger, secondInteger) {
     // console.log(firstInteger + secondInteger)
     // return firstInteger + secondInteger
     // return "hello"
@@ -12,4 +12,32 @@ function addTwoIntegers(firstInteger, secondInteger) {
 
 console.log( addTwoIntegers(12, "13") );
 console.log( addTwoIntegers(1920, 85) );
-console.log( 0 === "0" )
+console.log( 0 === "0" ) */
+
+function loadEvent(eventObject) {
+ //   console.log(eventObject)
+    const rootElement = document.getElementById("root");
+    //console.dir(document);
+    //console.dir(rootElement);
+    console.dir(rootElement)
+
+    const listOfSectionElements = document.querySelectorAll("section")
+    console.log(listOfSectionElements)
+    //rootElement.classList.add("newclass")
+    /* listOfSectionElements[0].classList.add("newclass")
+    listOfSectionElements[1].classList.add("newclass")
+    listOfSectionElements[2].classList.add("newclass")
+    listOfSectionElements[3].classList.add("newclass") */
+
+    
+    let anchors = "";
+    
+    for (const sectionElement of listOfSectionElements) {
+        //sectionElement.classList.add("newclass");
+        //anchors = anchors + `<a>${ sectionElement.id }</a>`
+        anchors += `<a href="#${sectionElement.id}">${ sectionElement.id }</a>`;
+    }
+    console.log(anchors);
+    rootElement.insertAdjacentHTML("afterbegin", `<header>${ anchors }</header>`)
+}
+window.addEventListener("load", loadEvent);
