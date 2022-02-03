e335d8ce938eb5ff45aa673455db87cf2c78cd98
windiewhale <feketebodza@gmail.com>
2022-02-03 16:02:10 +0100

    script excercises

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,15 @@
-function addToIntegers(firstInteger, secondInteger) {
-    console.log(firstInteger + secondInteger)
-
+function addTwoIntegers(firstInteger, secondInteger) {
+    // console.log(firstInteger + secondInteger)
+    // return firstInteger + secondInteger
+    // return "hello"
+    if (typeof firstInteger === "number" && typeof secondInteger === "number") {
+        return firstInteger + secondInteger
+    }
+    else {
+        return "valamelyik argumentum nem szám, ezért a művelet nem elvégezhető"
+    } 
 }
-addToIntegers(12, 13);
-addToIntegers(1920, 85);
+
+console.log( addTwoIntegers(12, "13") );
+console.log( addTwoIntegers(1920, 85) );
+console.log( 0 === "0" )
