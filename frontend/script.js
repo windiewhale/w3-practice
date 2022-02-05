9a983182f0fe50fbb473bf421535a77630d21c6b
windiewhale <feketebodza@gmail.com>
2022-02-05 11:19:30 +0100

    positioning examples, exam excersize

--- frontend/script.js
+++ frontend/script.js
@@ -1,43 +1,6 @@
-/* function addTwoIntegers(firstInteger, secondInteger) {
-    // console.log(firstInteger + secondInteger)
-    // return firstInteger + secondInteger
-    // return "hello"
-    if (typeof firstInteger === "number" && typeof secondInteger === "number") {
-        return firstInteger + secondInteger
-    }
-    else {
-        return "valamelyik argumentum nem szám, ezért a művelet nem elvégezhető"
-    } 
+function returnParam(parameter) {
+   // console.log(parameter)
+   return `<h1>${parameter}</h1>`
 }
-
-console.log( addTwoIntegers(12, "13") );
-console.log( addTwoIntegers(1920, 85) );
-console.log( 0 === "0" ) */
-
-function loadEvent(eventObject) {
- //   console.log(eventObject)
-    const rootElement = document.getElementById("root");
-    //console.dir(document);
-    //console.dir(rootElement);
-    console.dir(rootElement)
-
-    const listOfSectionElements = document.querySelectorAll("section")
-    console.log(listOfSectionElements)
-    //rootElement.classList.add("newclass")
-    /* listOfSectionElements[0].classList.add("newclass")
-    listOfSectionElements[1].classList.add("newclass")
-    listOfSectionElements[2].classList.add("newclass")
-    listOfSectionElements[3].classList.add("newclass") */
-
-    
-    let anchors = "";
-    
-    for (const sectionElement of listOfSectionElements) {
-        //sectionElement.classList.add("newclass");
-        //anchors = anchors + `<a>${ sectionElement.id }</a>`
-        anchors += `<a href="#${sectionElement.id}">${ sectionElement.id }</a>`;
-    }
-    console.log(anchors);
-    rootElement.insertAdjacentHTML("afterbegin", `<header>${ anchors }</header>`)
-}
-window.addEventListener("load", loadEvent);
+//returnParam(1999)
+console.log( returnParam("argument") )
